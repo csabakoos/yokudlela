e00c7d3f7aaf32234f3b81faa23406c694b8137d
csabakoos <csaba.koos@yahoo.com>
2022-03-15 19:23:55 +0100

    Changes.

--- haccp/src/main/java/hu/yokudlela/haccp/rest/WasteController.java
+++ haccp/src/main/java/hu/yokudlela/haccp/rest/WasteController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -35,10 +36,23 @@ public class WasteController {
             @ApiResponse(responseCode = "200", description = "The requested supply record",
                     content = { @Content(mediaType = "application/json",
                             schema = @Schema(implementation = StorageControl.class)) }),
+            @ApiResponse(responseCode = "403", description = "Missing permission",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "401", description = "Token expired",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "302", description = "Login required, redirecting to login page",
+                    content = { @Content(mediaType = "application/json") }),
             @ApiResponse(responseCode = "500", description = "Unsuccessful query",
                     content = { @Content(mediaType = "application/json") })
     })
-    @Operation(summary = "Request supply record by id")
+    @Operation(
+            summary = "Request supply record by id",
+            security = {
+                    @SecurityRequirement(name = "apikey",scopes = {"haccp"}),
+                    @SecurityRequirement(name = "openid",scopes = {"haccp"}),
+                    @SecurityRequirement(name = "oauth2",scopes = {"haccp"})
+            }
+    )
     @GetMapping(path = "/getRecordById/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public WasteControl getRecordById(
             @Parameter(description="Record's id", required = true, example = "0x3FFF000000000000")
@@ -50,10 +64,23 @@ public class WasteController {
             @ApiResponse(responseCode = "200", description = "New record added successfully",
                     content = { @Content(mediaType = "application/json",
                             schema = @Schema(implementation = StorageControl.class)) }),
+            @ApiResponse(responseCode = "403", description = "Missing permission",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "401", description = "Token expired",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "302", description = "Login required, redirecting to login page",
+                    content = { @Content(mediaType = "application/json") }),
             @ApiResponse(responseCode = "500", description = "Record already exists.",
                     content = { @Content(mediaType = "application/json") })
     })
-    @Operation(summary = "Add new storage control record.")
+    @Operation(
+            summary = "Add new storage control record.",
+            security = {
+                    @SecurityRequirement(name = "apikey",scopes = {"haccp"}),
+                    @SecurityRequirement(name = "openid",scopes = {"haccp"}),
+                    @SecurityRequirement(name = "oauth2",scopes = {"haccp"})
+            }
+    )
     @PostMapping(path = "/addRecord", produces = MediaType.APPLICATION_JSON_VALUE)
     public WasteControl add(@Parameter(description = "The new record",required = true) @RequestBody(required = true) WasteControl record) throws NoSuchElementException, InstanceAlreadyExistsException {
         this.wasteService.addRecord(record);
@@ -63,10 +90,23 @@ public class WasteController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Record deleted succesfully",
                     content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "403", description = "Missing permission",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "401", description = "Token expired",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "302", description = "Login required, redirecting to login page",
+                    content = { @Content(mediaType = "application/json") }),
             @ApiResponse(responseCode = "500", description = "No such record to delete",
                     content = { @Content(mediaType = "application/json") })
     })
-    @Operation(summary = "Delete a storage record")
+    @Operation(
+            summary = "Delete a storage record",
+            security = {
+                    @SecurityRequirement(name = "apikey",scopes = {"haccp"}),
+                    @SecurityRequirement(name = "openid",scopes = {"haccp"}),
+                    @SecurityRequirement(name = "oauth2",scopes = {"haccp"})
+            }
+    )
     @DeleteMapping(path = "/deleteRecord/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public void deleteRecord(
             @Parameter(description = "Storage record's id", required = true, example = "0x3FFF000000000000")
